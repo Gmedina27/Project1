b9c9adef5ab80fdb2679b36db7c778044f87e0cd
Gusta <Gusta@DESKTOP-MSFL281>
2023-04-05 19:51:13 -0600

    Initial Commit

---
+++ src/Demo/Test.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class Test {
+	
+  public static void main(String[] args) {
+	System.out.println("TEst.....");
+	
+	
+ System.out.println("Github Lecture....");
+	  
+	  
+	  
+}
+
+}
